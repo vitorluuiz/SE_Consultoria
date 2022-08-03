f9151e06eb0f8116dee41aba705e214e652ba2b5
Vitor <vitor.sesi21@gmail.com>
2022-08-03 16:52:15 -0300

    LoginCadastroResponsivel Finalizadas

--- src/pages/LoginCadastro/Cadastro.jsx
+++ src/pages/LoginCadastro/Cadastro.jsx
@@ -1,35 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from '../../components/footer.js'
 import '../../assets/css/loginCadastro.css'
 import MaskedInput from '../../assets/jsExtentions/MaskedInput/maskedInput.js'
+import axios from 'axios';
+
 export default function Cadastro() {
+
+    const [Nome, setNome] = useState('');
+    const [Telefone, setTelefone] = useState('');
+    const [Senha, setSenha] = useState('');
+    const [ContraSenha, setContraSenha] = useState('');
+
     return (
         <div>
-            <div className="fundoLogin column">
+            <div className="fundo_login column">
 
-                <div className="bloco_Login__div containerBox column alinhado">
-                    <form className="form_Login column alinhado">
-                        <div className="apoioForm_Login__div column">
-                            <label for="telefone">Nome completo</label>
-                            <input className="input_Form" id="telefone"></input>
+                <div className="bloco_login__div containerBox column alinhado" id="cadastro">
+
+                    <form className="form_login column alinhado" id="cadastro">
+
+                        <div className="inputs_apoio" id="cadastro">
+                            <div className="input column" id="cadastro">
+                                <label for="nome">Nome de usuário</label>
+                                <input id="nome" value={Nome} onChange={(e) => setNome(e.target.value)}></input>
+                            </div>
+
+                            <div className="input column" id="cadastro">
+                                <label for="telefone">Telefone</label>
+                                <MaskedInput mask="(99) 99999-9999" placeholder="(DDD) 98765-4321" id="telefone" value={Telefone} onChange={(e) => setTelefone(e.target.value)}></MaskedInput>
+                            </div>
+
+                            <div className="input column" id="cadastro">
+                                <label for="senha">Senha</label>
+                                <input type="password" id="senha" value={Senha} onChange={(e) => setSenha(e.target.value)}></input>
+                            </div>
+
+                            <div className="input column" id="cadastro">
+                                <label for="contraSenha">Confirme a senha</label>
+                                <input type="password" id="contraSenha" value={ContraSenha} onChange={(e) => setContraSenha(e.target.value)}></input>
+                            </div>
+
                         </div>
 
-                        <div className="apoioForm_Login__div column">
-                            <label for="telefone">Telefone</label>
-                            <MaskedInput className="input_Form" mask="(99) 99999-9999" id="telefone"></MaskedInput>
-                        </div>
-
-                        <div className="apoioForm_Login__div column">
-                            <label for="telefone">Senha</label>
-                            <input className="input_Form" type="password" id="telefone"></input>
-                        </div>
-
-                        <div className="apoioForm_Login__div column">
-                            <label for="senha">Confirme a senha</label>
-                            <input className="input_Form" type="password" id="senha"></input>
-                        </div>
-                        <buttom id="btnEntrar__Login" className="btnPressionavel">Cadastro</buttom>
+                        <buttom id="btnEntrar_cadastro" className="btnPressionavel">Cadastro</buttom>
                     </form>
+
+                </div>
+
+                <div className="boxInfo container column centrado" id="cadastro">
+                    <h2>Por que preciso realizar um cadastro?</h2>
+                    <p>Seu cadastro é opicional, e necessário apenas para assinalar interesse em determinados imóveis no qual tenha se interessado. Tendo assim, maior facilidade em analizar vários imóveis ao mesmo tempo, e futuramente escolher a oportunidade que mais se adequa ao seu perfil.
+Seus dados serão estritamente utilizados a fim de diferenciar você dos outros usuários da rede, e não podem ser processados ou vendidos para terceiros. Além do sigilo e da privacidade no armazenamento dos dados sensíveis, seguindo as ordens vigentes da LGPD (Lei geral de proteção de dados).</p>
                 </div>
 
             </div>
--- src/pages/LoginCadastro/Login.jsx
+++ src/pages/LoginCadastro/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from '../../components/footer.js'
 import { Link } from 'react-router-dom'
 import '../../assets/css/loginCadastro.css'
@@ -6,25 +6,37 @@ import Logo from '../../assets/img/logos/brancaNegativa.png'
 import MaskedInput from "../../assets/jsExtentions/MaskedInput/maskedInput.js";
 
 export default function Login() {
+
+    const [Telefone, setTelefone] = useState('');
+    const [Senha, setSenha] = useState('');
+
     return (
         <div>
-            <div className="fundoLogin column">
+            <div className="fundo_login column">
 
-                <div className="bloco_Login__div containerBox column alinhado">
-                    <img id="logo_Login" src={Logo} alt='Logo da SE Consultoria de Imóveis' />
-                    <form className="form_Login column alinhado">
-                        <div className="apoioForm_Login__div column">
-                            <label for="telefone">Telefone</label>
-                            <MaskedInput className="input_Form" name="tel" mask="(99) 99999-9999" id="telefone"></MaskedInput>
+                <div className="bloco_login__div containerBox column alinhado">
+
+                    <img id="logo_login" src={Logo} alt='Logo da SE Consultoria de imóveis' />
+
+                    <form id="login" className="form column alinhado">
+
+                        <div id="login" className="inputs_apoio">
+                            <div id="login" className="input column">
+                                <label for="telefone">Telefone</label>
+                                <MaskedInput id="telefone" mask="(99) 99999-9999" placeholder="(DDD) 98765-4321" value={Telefone} onChange={(e) => setTelefone(e.target.value)} ></MaskedInput>
+                            </div>
+
+                            <div id="login" className="input column">
+                                <label for="senha">Senha</label>
+                                <input type="password" id="senha" value={Senha} onChange={(e) => setSenha(e.target.value)}></input>
+                            </div>
                         </div>
 
-                        <div className="apoioForm_Login__div column">
-                            <label for="senha">Senha</label>
-                            <input className="input_Form" type="password" id="senha"></input>
-                        </div>
                         <Link to="/Cadastrar" className="pressionavel">Ainda não tenho conta</Link>
-                        <buttom id="btnEntrar__Login" className="btnPressionavel">Entre</buttom>
+                        <buttom id="btnEntrar_login" className="btnPressionavel">Entre</buttom>
+
                     </form>
+
                 </div>
 
             </div>
